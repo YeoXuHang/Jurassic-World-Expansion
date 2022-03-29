05e8e5225274cb8f780d0a9df023072d45409fa8
YeoXuHang <xuhang.yeo@gmail.com>
2022-03-29 18:16:03 +0800

    Now T-rex works

--- src/main/java/com/rayrindu/jwe/event/ClientModEvents.java
+++ src/main/java/com/rayrindu/jwe/event/ClientModEvents.java
@@ -6,7 +6,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
 
 @Mod.EventBusSubscriber(modid = JweMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ClientModEvents {
