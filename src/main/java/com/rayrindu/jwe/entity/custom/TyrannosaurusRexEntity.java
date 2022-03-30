d4a2f5058041c6fc58bff5493c1e394fa3fdb1dc
YeoXuHang <xuhang.yeo@gmail.com>
2022-03-30 11:01:53 +0800

    T-rex

--- src/main/java/com/rayrindu/jwe/entity/custom/TyrannosaurusRexEntity.java
+++ src/main/java/com/rayrindu/jwe/entity/custom/TyrannosaurusRexEntity.java
@@ -1,5 +1,6 @@
 package com.rayrindu.jwe.entity.custom;
 
+import com.rayrindu.jwe.entity.JweModEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
@@ -7,16 +8,21 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 import javax.annotation.Nullable;
+import java.util.Objects;
 
 public class TyrannosaurusRexEntity extends Animal {
      public TyrannosaurusRexEntity(EntityType<? extends Animal> entityType, Level level) {
@@ -28,29 +34,32 @@ public class TyrannosaurusRexEntity extends Animal {
                 .add(Attributes.MAX_HEALTH, 20.0D)
                 .add(Attributes.ATTACK_DAMAGE, 3.0f)
                 .add(Attributes.ATTACK_SPEED, 2.0f)
-                .add(Attributes.MOVEMENT_SPEED, 0.3f).build();
+                .add(Attributes.MOVEMENT_SPEED, 0.3f)
+                .add(Attributes.FOLLOW_RANGE, 100D)
+                .build();
     }
 
     protected void registerGoals() {
+        this.goalSelector.addGoal(0, new MeleeAttackGoal(this, 1.0D, true));
         this.goalSelector.addGoal(1, new FloatGoal(this));
-        // this.goalSelector.addGoal(1, new SitWhenOrderedToGoal(this));
-        this.goalSelector.addGoal(2, new PanicGoal(this, 1.25D));
-        // this.goalSelector.addGoal(3, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
         this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
         this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(6, (new HurtByTargetGoal(this)).setAlertOthers());
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
     @Nullable
     @Override
-    public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
-        return null;
+    public AgeableMob getBreedOffspring(ServerLevel serverWorld, AgeableMob ageable) {
+        TyrannosaurusRexEntity retval = JweModEntityTypes.T_REX.get().create(serverWorld);
+        retval.finalizeSpawn(serverWorld, serverWorld.getCurrentDifficultyAt(retval.blockPosition()), MobSpawnType.BREEDING, null, null);
+        return retval;
     }
 
-
-    protected void playStepSound(BlockPos pos, BlockState blockIn) {
-        this.playSound(SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, 0.15F, 1.0F);
+    @Override
+    public boolean isFood(ItemStack stack) {
+        return Objects.equals(Items.COOKED_BEEF, stack.getItem());
     }
 
     protected SoundEvent getAmbientSound() {
@@ -66,6 +75,6 @@ public class TyrannosaurusRexEntity extends Animal {
     }
 
     protected float getSoundVolume() {
-        return 0.2F;
+        return 5F;
     }
 }
